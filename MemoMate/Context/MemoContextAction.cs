7ebbd9d5a7bac3c085e45d337c437f98d014f9cf
Arimochi <156779510+ArimochiXIV@users.noreply.github.com>
2024-01-22 23:21:50 -0600

    Change color/verbiage if a player has a memo or not.

--- MemoMate/Context/MemoContextAction.cs
+++ MemoMate/Context/MemoContextAction.cs
@@ -1,36 +1,36 @@
 ﻿using Dalamud.ContextMenu;
 using Dalamud.Game.Text;
 using Dalamud.Game.Text.SeStringHandling;
+using MemoMate.Data;
 using MemoMate.Windows;
 
 namespace MemoMate.Context;
 
 public static class MemoContextAction
 {
-    // TODO: Add color difference for when person has a memo stored.
-    private static SeString ActionName
-    {
-        get
-        {
-            var seStringBuilder = new SeStringBuilder();
-
-            seStringBuilder.AddUiForeground(31);
-            seStringBuilder.Append(SeIconChar.BoxedLetterM.ToIconString());
-            seStringBuilder.AddUiForegroundOff();
-            seStringBuilder.Append(" Open Memo");
-            
-            return seStringBuilder.BuiltString;
-        }
-    }
+    private const ushort NoMemoColor = 31;
+    private const ushort HasMemoColor = 60;
 
     private static DalamudContextMenu _contextMenu;
-
-    private static GameObjectContextMenuItem MenuItem => new (ActionName, OnClick);
 
     public static void Initialize()
     {
         _contextMenu = new DalamudContextMenu(Services.Instance.PluginInterface);
         _contextMenu.OnOpenGameObjectContextMenu += DecideAddItem;
+    }
+
+    private static SeString GetActionName(string name, uint worldId)
+    {
+        var hasMemo = MemoDb.Exists(name, worldId);
+        
+        var seStringBuilder = new SeStringBuilder();
+
+        seStringBuilder.AddUiForeground(hasMemo ? HasMemoColor : NoMemoColor);
+        seStringBuilder.Append(SeIconChar.BoxedLetterM.ToIconString());
+        seStringBuilder.AddUiForegroundOff();
+        seStringBuilder.Append($" {(hasMemo ? "Open" : "New")} Memo");
+            
+        return seStringBuilder.BuiltString;
     }
 
     private static void DecideAddItem(GameObjectContextMenuOpenArgs args)
@@ -47,8 +47,9 @@ public static class MemoContextAction
             return;
         if (args.Text.TextValue == localPlayer.Name.TextValue && args.ObjectWorld == localPlayer.HomeWorld.Id)
             return;
-        
-        args.AddCustomItem(MenuItem);
+
+        var name = GetActionName(args.Text.TextValue, args.ObjectWorld);
+        args.AddCustomItem(new GameObjectContextMenuItem(name, OnClick));
     }
 
     private static void OnClick(GameObjectContextMenuItemSelectedArgs args)
